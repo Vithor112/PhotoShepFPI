e60af5c5577774b707ad49ee6c648cca5a84fe4a
Vithor <vithorpileco@gmail.com>
2023-01-12 01:56:05 -0300

    Add QT files

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "Image.hpp"
+#include "mainwindow.h"
+#include <QApplication>
 
-
-int main(void) {
-    Image x("sky.jpeg"); 
-    for (int i = 0; i < 50; i++) {
-        Image y = x.GetGrayImage(1).quantizeImage(i); 
-        y.SaveJPEGImg(std::to_string(i), 100); 
-    }
-
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
